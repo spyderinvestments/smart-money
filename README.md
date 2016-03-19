62d202f4d509a1e509b51a8ca732e06972dbff82
Michael Liang <michaelpliang@gmail.com>
2016-03-19 03:02:45 -0700

    big commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# Smart Money
+
+>Work in Progress
+
+## Table of Contents
+
+1. [Usage](#Usage)
+1. [Scope](#Scope)
+    1. [Installing Dependencies](#installing-dependencies)
+    1. [Tasks](#tasks)
+1. [Contributing](#contributing)
+
+## Scope:
+
+1. Be able to add rent, household bills, etc.
+2. Visualize monthly expense with d3
+3. Have a budget manager
+4. Integrate with FB and venmo
+5. use mixpanel and google analytics
+6. cross browser checking
+7. jshint
+
+### Features:
+
+
+### Todo:
+
+- [ ] Watch some angular tutorials to get a better feel for how file structure is supposed to be
+
+- [x] Tab of resources to study/practice
+
+- [ ] Custom 404 page
+
+This is a MEAN Stack Scaffold which includes:
+
+- Express
+- Mongoose
+- Angular
+- Angular UI Router
+- jQuery
+- Materialize
+
+
+### Installation
+
+1. Run an `npm install` and a `bower install`.
+2. Export the required JWT_SECRET environment variable.
+3. Start the server.
---
+++ gulpfile.js
@@ -0,0 +1,71 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var ignore = require('gulp-ignore');
+var del = require('del');
+var gutil = require('gulp-util');
+var minifyCss = require('gulp-minify-css');
+var ngmin = require('gulp-ngmin');
+var uglify = require('gulp-uglify');
+var concat = require("gulp-concat");
+var babel = require('gulp-babel');
+
+var dirs = {
+  src: {
+    js: "client/app/**/*.js",
+    scss: "client/app/app.scss",
+    html: "client/app/**/*.html",
+    lib: "client/assets/**/*"
+  },
+  out: {
+    html: 'dist/',
+    css: 'dist/',
+    lib: 'dist/'
+  }
+}
+
+gulp.task('default', ['clean', 'sass', 'assets', 'bundle', 'watch']);
+gulp.task('buildDontWatch', ['clean', 'sass', 'assets', 'bundle']);
+
+gulp.task('clean', function(){
+  del('dist/*');
+})
+
+gulp.task('sass', function(done) {
+  gulp.src(dirs.src.scss)
+    .pipe(sass())
+    .on('error', sass.logError)
+    .pipe(minifyCss())
+    .pipe(gulp.dest(dirs.out.css))
+    .on('end', done);
+});
+
+gulp.task('assets',['sass'], function() {
+  gulp.src(dirs.src.lib)
+    .pipe(gulp.dest(dirs.out.lib))
+});
+
+gulp.task('bundle', function(done) {
+  gulp.src('src/app/index.html')
+    .pipe(gulp.dest('dist'))
+  gulp.src(dirs.src.html)
+    .pipe(ignore('index.html'))
+    .pipe(ignore('app.scss'))
+    .pipe(ignore('my_scss'))
+    .pipe(gulp.dest('dist/js'))
+  gulp.src(dirs.src.js)
+    .pipe(babel({
+      presets: ['es2015']
+    }))
+    .pipe(concat('bundle.js'))
+    .pipe(ngmin({dynamic: true}))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js'))
+    .on('end', done)
+});
+
+gulp.task('watch', function() {
+  gulp.watch(dirs.src.html, ['assets', 'bundle']);
+  gulp.watch(dirs.src.lib, ['assets']);
+  gulp.watch(dirs.src.scss, ['sass']);
+  gulp.watch(dirs.src.js, ['buildDontWatch']);
+});
